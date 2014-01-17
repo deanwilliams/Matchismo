8963856c87578685b8494c8c431d845651b55df5
Dean Williams <dean.williams1980@gmail.com>
2014-01-17 14:09:28 +0000

    More elegant solution plus fixes niggles with still flipping after the deck is empty

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -7,24 +7,30 @@
 //
 
 #import "CardGameViewController.h"
+#import "Deck.h"
 #import "PlayingCardDeck.h"
-#import "PlayingCard.h"
+#import "Card.h"
 
 @interface CardGameViewController ()
 
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
 
-@property (strong, nonatomic) PlayingCardDeck *deck;
+@property (strong, nonatomic) Deck *deck;
 
 @end
 
 @implementation CardGameViewController
 
-- (PlayingCardDeck *)deck
+- (Deck *)deck
 {
-    if (!_deck) _deck = [[PlayingCardDeck alloc] init];
+    if (!_deck) _deck = [self createDeck];
     return _deck;
+}
+
+- (Deck *)createDeck
+{
+    return [[PlayingCardDeck alloc] init];
 }
 
 - (void)setFlipCount:(int)flipCount
@@ -40,16 +46,16 @@
         [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
                           forState:UIControlStateNormal];
         [sender setTitle:@"" forState:UIControlStateNormal];
+        self.flipCount++;
     } else {
-        [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
-                          forState:UIControlStateNormal];
-        PlayingCard *card = (PlayingCard *)[self.deck drawRandomCard];
-        if (!card) {
-            NSLog(@"Card is nil");
+        Card *card = [self.deck drawRandomCard];
+        if (card) {
+            [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
+                              forState:UIControlStateNormal];
+            [sender setTitle:[card contents] forState:UIControlStateNormal];
+            self.flipCount++;
         }
-        [sender setTitle:[card contents] forState:UIControlStateNormal];
     }
-    self.flipCount++;
 }
 
 
